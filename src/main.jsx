c0c6dcafbd2f5882280db356be6f7d7a98d49290
Armanii <kaiallapico@gmail.com>
2026-03-12 00:07:29 +1000

    refactor: Correct script path and log order
    Update the script path in index.html to use a relative path for the module.
    Reorder the console log in main.jsx to appear after imports.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
+console.log('Main.jsx is loading...');
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
 import './index.css';
-
-console.log('Main.jsx is loading...');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
