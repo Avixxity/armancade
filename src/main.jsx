b0e8f0df9b71f4ef531169197eb2def5a1991f85
Armanii <kaiallapico@gmail.com>
2026-03-11 23:35:32 +1000

    feat: Add loading log to main.jsx
    Logs a message to the console when the main application entry point is loaded. This can aid in debugging startup issues.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import {createRoot} from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+console.log('Main.jsx is loading...');
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
